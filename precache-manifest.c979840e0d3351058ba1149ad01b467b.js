e4766fdd85216724d57f0fdf6902776051b80c2f
Nicole <qwepoi9633@gmail.com>
2019-06-20 23:09:39 +0800

    npm run build 打包

---
+++ precache-manifest.c979840e0d3351058ba1149ad01b467b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "748a51a3bd0d6c9046b750d141805b7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e87562e2e84fd10c2b5",
+    "url": "/static/css/2.a19be74d.chunk.css"
+  },
+  {
+    "revision": "aab88204efa2ddd736e9",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "1e87562e2e84fd10c2b5",
+    "url": "/static/js/2.c2638a83.chunk.js"
+  },
+  {
+    "revision": "aab88204efa2ddd736e9",
+    "url": "/static/js/main.75105c9c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
